6ebb9375eea6589a738c9733b662fb88ecccdd49
Ayush Raj <email@ayu943189.com>
2025-04-29 20:30:07 +0530

    first commit

---
+++ library/src/main/java/com/suddenh4x/ratingdialog/dialog/DialogType.kt
@@ -0,0 +1,10 @@
+package com.suddenh4x.ratingdialog.dialog
+
+import java.io.Serializable
+
+enum class DialogType : Serializable {
+    RATING_OVERVIEW,
+    RATING_STORE,
+    FEEDBACK_MAIL,
+    FEEDBACK_CUSTOM,
+}
